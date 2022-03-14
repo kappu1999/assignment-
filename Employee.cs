89a18da06443cb6b8bfa2798d7ddbe5063be0e7e
kappu1999 <74296849+kappu1999@users.noreply.github.com>
2022-03-14 17:54:45 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Employee // class
+    {
+        private int ID;
+        private string Name;
+        private string DepartmentName;
+        public Employee(int ID,string Name,string DepartmentName)   //constructor
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.DepartmentName = DepartmentName;
+        }
+        public int GetId()
+        {
+        return this.ID;
+        }
+        public string GetName()
+        {
+        return this.Name;
+        }
+        public string GetDepartmentName()
+        {
+        return this.DepartmentName;
+        }
+        static void Main(string[] args)
+        {
+            Employee E1 = new Employee(11,"kapil","IT");
+            
+            Console.WriteLine("employee id is {0}",E1.GetId());
+            Console.WriteLine("employee name is {0}",E1.GetName());
+            Console.WriteLine("employee department name is {0}",E1.GetDepartmentName());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Assignment_1
+{
+    public class Program{
+
+        static void methodCalled(object sender, string functionName){
+            Console.WriteLine("{0} method called",functionName);
+        }
+        static void Main(string[] args){
+            int Id = 0;
+            string Name = "";
+            string DepartmentName = "";
+            Employee emp;
+            
+            try{
+                Console.WriteLine("Enter Your ID: ");
+                Id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Your Name: ");
+                Name = Console.ReadLine();
+                if(String.IsNullOrEmpty(Name)){
+                    throw new ArgumentException("Name can't be empty");
+                }
+
+                Console.WriteLine("Enter Your Department Name: ");
+                DepartmentName = Console.ReadLine();
+                if(String.IsNullOrEmpty(DepartmentName))
+                {
+                    throw new ArgumentException("DepartmentName Name can't be empty");
+                }
+                emp = new Employee(Id,Name,DepartmentName);
+                emp.getDetailsCalled += methodCalled;
+                Console.WriteLine("Ypur ID: {0}",emp.getId());
+                Console.WriteLine("Your Name:{0}",emp.getName());
+                Console.WriteLine("Your DepartmentName:{0}",emp.getDepartmentName());
+                emp.update(201);
+                System.Console.WriteLine(emp.getId());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }   
+        }
+    }
+}
